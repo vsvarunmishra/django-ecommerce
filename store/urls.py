b0add9b4c5881fed9e955b865684700aacdd9e10
vsvarunmishra <vsvarunmishra@gmail.com>
2025-08-06 20:57:49 +0530

    pagination and search

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.store,name='store'),
-    path('<str:category_slug>/',views.store,name='product_by_slug'),
-    path('<str:category_slug>/<slug:product_slug>/',views.product_details,name='product_details'),
+    path('category/<str:category_slug>/',views.store,name='product_by_slug'),
+    path('category/<str:category_slug>/<slug:product_slug>/',views.product_details,name='product_details'),
+    path('search/',views.search,name='search'),
 ]
